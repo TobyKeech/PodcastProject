0eda6bdafed57f9606bc195fe1343ca0cebdf1ca
Wenjing2023 <wenjing2018uk@gmail.com>
2023-05-01 15:52:41 +0100

    wenjings branch

--- client/src/containers/PodcastContainer.js
+++ client/src/containers/PodcastContainer.js
@@ -4,92 +4,81 @@ import NavBar from "../components/NavBar";
 import SearchList from "../components/SearchList";
 import FavList from "../components/FavList";
 import { getPodSeries } from "../services/APIService";
-import './PodcastContainer.css'
+// import './PodcastContainer.css'
 
 import {
-    getFavourites,
-    postFavourite,
-    putFavourite,
-    deleteFavourite,
+  getFavourites,
+  postFavourite,
+  putFavourite,
+  deleteFavourite,
 } from "../services/PodcastService";
 
 const PodcastContainer = () => {
-    const [dbFavPods, setDbFavPods] = useState([]);
-    const [podcastsData, setPodcastsData] = useState([]);
+  const [dbFavPods, setDbFavPods] = useState([]);
+  const [podcastsData, setPodcastsData] = useState([]);
 
-    useEffect(() => getDbFavPods, []);
+  useEffect(() => getDbFavPods, []);
 
+  const getDbFavPods = async () => {
+    const favouritesData = await getFavourites();
+    setDbFavPods(favouritesData);
+  };
 
-    if (!podcastsData) {
-        fetchAllPodcastData()
-    }
+  const fetchAllPodcastData = async () => {
+    const allPodcastPromises = dbFavPods.map(async (dbPodcast) => {
+      const onePodcastData = await fetchOnePodcastData(dbPodcast.uuid);
+      return onePodcastData;
+    });
 
-    const getDbFavPods = async () => {
-        const favouritesData = await getFavourites();
-        setDbFavPods(favouritesData);
-    };
+    const allPodcastsPromise = Promise.all(allPodcastPromises);
+    const allPodcastsData = await allPodcastsPromise;
+    setPodcastsData(allPodcastsData);
+  };
 
-    const fetchAllPodcastData = async () => {
+  const fetchOnePodcastData = async (uuid) => {
+    const data = await getPodSeries(uuid);
+    return data;
+  };
 
-        const allPodcastPromises = dbFavPods.map( async (dbPodcast) => {
-            const onePodcastData = await fetchOnePodcastData(dbPodcast.uuid)
-            return onePodcastData
-        })
+  fetchAllPodcastData();
 
-        const allPodcastsPromise = Promise.all(allPodcastPromises)
-        const allPodcastsData = await allPodcastsPromise
-        setPodcastsData(allPodcastsData)
-    };
+  const removeFavourite = (uuidToDelete) => {
+    const idToDelete = dbFavPods.find((favPod) => {
+      return favPod.uuid === uuidToDelete;
+    })._id;
 
-   
+    deleteFavourite(idToDelete);
+    setDbFavPods(
+      dbFavPods.filter((favPod) => {
+        return favPod.uuid !== uuidToDelete;
+      })
+    );
+  };
+  //   data is empty and db has everything
 
-    const fetchOnePodcastData = async (uuid) => {
-        const data = await getPodSeries(uuid);
-        return data
-    }
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<SearchList />} />
 
-
-
-
-
-
-    const removeFavourite = (uuidToDelete) => {
-        const idToDelete = dbFavPods.find((favPod) => {
-            return favPod.uuid === uuidToDelete;
-        })._id;
-
-        deleteFavourite(idToDelete);
-        setDbFavPods(
-            dbFavPods.filter((favPod) => {
-                return favPod.uuid !== uuidToDelete;
-            })
-        );
-    };
-
-    return (
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route exact path="/" element={<SearchList />} />
-
-                <Route
-                    path="/favourites"
-                    element={
-                        dbFavPods ? (
-                            <FavList
-                            podcastsData={podcastsData}
-                                removeFavourite={removeFavourite}
-                            />
-                        ) : null
-                    }
-                />
-                {/* 
+        <Route
+          path="/favourites"
+          element={
+            podcastsData ? (
+              <FavList
+                podcastsData={podcastsData}
+                removeFavourite={removeFavourite}
+              />
+            ) : null
+          }
+        />
+        {/* 
         <Route path="/queue" element={<Queue />} />
         <Route path="liked" element={<Liked />} /> */}
-            </Routes>
-        </Router>
-    );
+      </Routes>
+    </Router>
+  );
 };
 
 export default PodcastContainer;
-
